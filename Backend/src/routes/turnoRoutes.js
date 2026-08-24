8724107f3c403596fa03d0309fd865a368dbe5e3
MaxLRxd <maximorojas002@gmail.com>
2026-08-24 20:57:03 -0300

    turnoRoutes actualizado

--- Backend/src/routes/turnoRoutes.js
+++ Backend/src/routes/turnoRoutes.js
@@ -8,5 +8,8 @@ router.use(verificarToken);
 router.post('/', verificarRol('paciente', 'operador'), turnoController.crear);
 router.patch('/:id/cancelar', verificarRol('paciente', 'operador', 'medico'), turnoController.cancelar);
 router.patch('/:id/atender', verificarRol('medico'), turnoController.atender);
+router.get('/paciente', verificarRol('paciente'), turnoController.listarPorPaciente);
+router.get('/medico', verificarRol('medico'), turnoController.listarPorMedico);
+router.get('/sede', verificarRol('operador'), turnoController.listarPorSede);
 
 module.exports = router;
